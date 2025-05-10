6b0f9e1bbc91efe41aec8ef93733a7b864ae148c
Samuel Kenzo Umezawa <kenzoumezawa@gmail.com>
2025-05-10 11:36:12 -0300

    Add project files.

---
+++ InventarioMed.Shared.Data/InventarioMedContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventarioMed.Shared.Data
+{
+    public class InventarioMedContext : DbContext
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=InventarioMed_DB;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
